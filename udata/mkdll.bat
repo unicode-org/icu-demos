a94d2c729a74081f42319ba1a37e774b82acb176
Markus Scherer <markus.icu@gmail.com>
1999-11-02 18:51:27 +0000

    [ICU-88] win32 common data dll: add copyright to dll header
    X-SVN-Rev: 152

--- udata/mkdll.bat
+++ udata/mkdll.bat
@@ -1,6 +1,11 @@
 @echo off
 if "%ICU_DATA%"=="" goto :error
-cl "/I..\..\icu\source\common" /GD "/Fe%ICU_DATA%icudata.dll" "%ICU_DATA%unames_dat.c" /link /DLL /NOENTRY /base:"0x4ad00000"
+
+cl "/I..\..\icu\source\common" /GD /c "%ICU_DATA%unames_dat.c"
+echo "/out:%ICU_DATA%icudata.dll">mkdll.tmp
+echo unames_dat.obj>>mkdll.tmp
+type mkdll.lk>>mkdll.tmp
+link @mkdll.tmp
 goto :end
 
 :error
