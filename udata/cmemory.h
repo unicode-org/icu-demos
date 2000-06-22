1f8dbd71700feedaaf8ed5a51829a1018fd86720
Steven R. Loomis <srl@icu-project.org>
2000-06-22 17:21:09 +0000

    [ICU-393] removing obsolete files
    X-SVN-Rev: 1639

--- udata/cmemory.h
+++
@@ -1,7 +0,0 @@
-/*******************************************************************************
- *
- *   Copyright (C) 1997-2000, International Business Machines
- *   Corporation and others.  All Rights Reserved.
- *
- *******************************************************************************/
-#error This file is obsolete. Please use common library.
--- udata/gennames.c
+++
@@ -1,7 +0,0 @@
-/*******************************************************************************
- *
- *   Copyright (C) 1999-2000, International Business Machines
- *   Corporation and others.  All Rights Reserved.
- *
- *******************************************************************************/
-#error This file is obsolete. Please icu/source/tools/gennames.
