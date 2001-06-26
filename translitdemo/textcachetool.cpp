04422608300820a926d1fb7bfa51bf1e9b59c79c
Alan Liu <alansliu@gmail.com>
2001-06-26 20:10:14 +0000

    [ICU-967] rename TextCacheTest to textcachetool and make into generic tool
    X-SVN-Rev: 5094

---
+++ translitdemo/textcachetool.cpp
@@ -0,0 +1,76 @@
+#include "TextCache.h"
+#include "util.h"
+
+void visitor(int32_t i, const UnicodeString& key, void* ccache) {
+    printf(" %ld: ", i);
+    util_fprintf(stdout, key);
+    printf(" => \"");
+    UnicodeString str;
+    ((TextCache*)ccache)->get(key, str);
+    util_fprintf(stdout, str);
+    printf("\"\n");
+}
+
+void usage() {
+    printf("Usage: ttt <cache-dir> [commands]\n");
+    printf(" <cache-dir> Path to cache\n");
+    printf("Commands:\n");
+    printf(" put <key> <value>\n");
+    printf(" get <key>\n");
+    printf(" rem <key>\n");
+    printf(" dump\n");
+    exit(1);
+}
+
+/**
+ * A simple tool for TextCache.  Used for testing TextCache
+ * and manipulating caches.
+ */
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) usage();
+
+    int argi = 1;
+
+    TextCache cache(argv[argi++]);
+
+    while (argi < argc) {
+        if (strcmp(argv[argi], "put") == 0) {
+            ++argi;
+            if ((argi+2) > argc) usage();
+            const char* key = argv[argi++];
+            const char* value = argv[argi++];
+            cache.put(key, value);
+        }
+        
+        else if (strcmp(argv[argi], "get") == 0) {
+            ++argi;
+            if ((argi+1) > argc) usage();
+            const char* key = argv[argi++];
+            UnicodeString str;
+            cache.get(key, str);
+            printf("%s => \"", key);
+            util_fprintf(stdout, str);
+            printf("\"\n");
+        }
+        
+        else if (strcmp(argv[argi], "rem") == 0) {
+            ++argi;
+            if ((argi+1) > argc) usage();
+            const char* key = argv[argi++];
+            cache.remove(key);
+        }
+
+        else if (strcmp(argv[argi], "dump") == 0) {
+            ++argi;
+            printf("dump:\n");
+            cache.visitKeys(visitor, &cache);
+        }
+
+        else {
+            usage();
+        }
+    }
+
+    return 0;
+}
---
+++ translitdemo/textcachetool.make
@@ -0,0 +1,49 @@
+
+ICU_DEFAULT_PREFIX=~/icu
+
+ifeq ($(strip $(ICU_PREFIX)),)
+  ICU_INC=$(ICU_DEFAULT_PREFIX)/share/icu/Makefile.inc
+else
+  ICU_INC=$(ICU_PREFIX)/share/icu/Makefile.inc
+endif
+ICUPATH=
+
+###########################################
+# HACK : TODO fix this
+ICU_INC=/usr/local/lib/icu/Makefile.inc
+###########################################
+
+include $(ICU_INC)
+
+# Name of your target
+TARGET=ttt
+
+# All object files (C or C++)
+OBJECTS=util.o uhash.o TextCache.o textcachetool.o
+
+CLEANFILES=*~ $(TARGET).out
+
+DEPS=$(OBJECTS:.o=.d)
+
+all: $(TARGET)
+
+.PHONY: all clean distclean check report
+
+distclean clean:
+	-test -z "$(CLEANFILES)" || rm -f $(CLEANFILES)
+	-$(RMV) $(OBJECTS) $(TARGET)
+	-$(RMV) $(DEPS)
+
+# Can change this to LINK.c if it is a C only program
+# Can add more libraries here. 
+$(TARGET): $(OBJECTS)
+	$(LINK.cc) -o $@ $^ $(ICULIBS)
+
+$(ICU_INC):
+	@echo "Please read the directions at the top of this file (Makefile)"
+	@echo "Can't open $(ICU_INC)"
+	@false
+
+ifneq ($(MAKECMDGOALS),distclean)
+-include $(DEPS)
+endif
