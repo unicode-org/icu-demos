87ec0ca985db9a9e54146729b4f5833bbfea9d41
Andy Heninger <andy.heninger@gmail.com>
2005-02-04 18:32:44 +0000

    [ICU-4276] updates for IBM GWA compliance
    X-SVN-Rev: 17180

--- scompare/scompare.cpp
+++ scompare/scompare.cpp
@@ -4,36 +4,10 @@
  *
  ********************************************************************
  * COPYRIGHT:
- * Copyright (c) 2004, International Business Machines Corporation and
+ * Copyright (c) 2004-2005, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************
  */
-
-//
-//  This cgi program is a little different from many.
-//  Rather than return a complete web page, it returns only the compare results themselves.
-//
-//  The html returned from here is loaded into a non-displaying frame in the browser,
-//  then JavaScript running on the browser extracts those results and places them
-//  into the displayed page.
-//
-//  A result is returned in this format:
-//    <p id="s1a">input string 1 after unescaping it</p>
-//    <p id="s1b">input string 2 after unescaping it</p>
-//    <p id="r1">--YY</p>
-//
-//   and continuing with [s2a, s2b, r2] [s3a, s3b, r3] ...
-//   for as many input strings as are found in the incoming POST data.
-//
-//   Within the rn strings, 
-//            r[0] : 'Y' if input strings are binary equal, otherwise '-'
-//            r[1] : 'Y' if input strings are case-insesitive equal.
-//            r[2] : 'Y' if input strings are canonically-equivalent equal
-//            r[3] : 'Y' if input strings are case / canon equiv equal.
-
-//  The script on the browser side picks up a value with InvisibleFramedDoc.getElementById("result-name")
-//    and, from there, can do whatever it wants with it.
-//
 
 
 #include <stdio.h>
@@ -45,6 +19,7 @@
 #include <unicode/unistr.h>
 #include <unicode/normlzr.h>
 #include <unicode/uchar.h>
+#include <unicode/ustdio.h>
 
 
 //
@@ -52,23 +27,22 @@
 //   the html form.
 //
 //    parameters:
-//       content  - char * pointer to the original POST data.
+//       pdata    - char * pointer to the original POST data.
 //       name     - char * name of the parameter to fetch.
 //    return
 //       UnicodeString containing the parameter, with
 //             - the html form escaping removed
 //             - converted from utf-8
-//             - ICU unescape applied
 //       If the parameter does not exist in the POST data, return
 //          a BOGUS string.
 //
-UnicodeString  getParam(const char *content, const char *name) {
+UnicodeString  getParam(const char *pdata, const char *name) {
     UnicodeString returnStr;
 
     char *namebuf=new char[strlen(name)+10];
     strcpy(namebuf, name);
     strcat(namebuf, "=");
-    char *start = strstr(content, namebuf);
+    char *start = strstr(pdata, namebuf);
     delete[] namebuf;
     if (start==0) {
         returnStr.setToBogus();
@@ -99,18 +73,50 @@ UnicodeString  getParam(const char *content, const char *name) {
         }
         cdata[dsti++] = c;
     }
-    cdata[dsti++] = 0;
+    cdata[dsti] = 0;
 
     // POST style encoding is now decoded, we have a char * utf-8 string.
     //  Slam it into the UnicodeString.
     returnStr = UnicodeString(cdata, dsti, "utf-8");
-    returnStr = returnStr.unescape();
     delete[] cdata;
 
     return returnStr;
  }
 
-  
+
+//
+//  storeField
+//      Store a string value into a %%field%% in the generated html.
+//      Html escape any '&' or '<' chars in the value.
+//
+void storeField(const char *fieldName, const UnicodeString &val, UnicodeString &content) {
+    UnicodeString vCopy(val);           
+    vCopy.findAndReplace("&", "&amp;"); 
+    vCopy.findAndReplace("<", "&lt;");
+    content.findAndReplace(fieldName, vCopy);
+}
+
+
+//
+//  StringToHex    Create a Hex representation of a UnicodeString.
+//                  Example:  "ABC" goes to "\u0041 \u0042 \u0043"
+//
+void toHex(UnicodeString &dest, const UnicodeString &src) {
+    int i;
+    int hexLen = 0;
+
+    dest.truncate(0);
+    UChar *bp = dest.getBuffer(src.length()*7 + 3);       // length per char is "\uxxxx " = 7 uchars.
+    for (i=0; i<src.length(); i=src.moveIndex32(i, 1)) {   //   (Supplementaries take less: 2 src chars -> 11 in dest)
+        UChar32  c = src.char32At(i);
+        if (c <= 0xffff) {
+            hexLen += u_sprintf(bp+hexLen, "\\u%04x ", c);
+        } else {
+            hexLen += u_sprintf(bp+hexLen, "\\U%08x ", c);
+        }
+    }
+    dest.releaseBuffer(hexLen);
+}
 
 //
 //  doCompare
@@ -119,121 +125,213 @@ UnicodeString  getParam(const char *content, const char *name) {
 //      The results are  wrapped in the appropriate html and written to stdout.
 //
 //      parameters:
-//          content:  the raw, unprocessed POST data submitted from the html form.
+//          postData: the raw, unprocessed POST data submitted from the html form.
 //                    The strings to be compared will be extracted from this.
-//          name1:    The name of the first string.
-//          name2:    The name of the second string
-//          rName:    The name with which to label the output string.
+//          content:  The html output page as put together from the template files,
+//                    ready for the result fields to be filled.
 //
-//      output (to stdout) looks like
-//          <p id="name1">the first input string</p>
-//          <p id="name2"the second input string</p>
-//          <p id="rName">----</p>
+//      output 
+//          The html content (the content string paramter) will have the %%field name%%
+//            fields replaced with the actual contents.
 //
-//      return true if name1 and name2 exist as parameters in the POST data
-//             false if either is missing.
 
-bool doCompare(const char *content, const char *name1, const char *name2, const char *rName) {
+void doCompare(const char *postData, UnicodeString &content) {
     UErrorCode    status = U_ZERO_ERROR;
 
-    UnicodeString s1 = getParam(content, name1);
-    UnicodeString s2 = getParam(content, name2);
+    // Fetch the two strings to be compared from the post data.
+    //
+    UnicodeString s1 = getParam(postData, "s1a");
+    UnicodeString s2 = getParam(postData, "s1b");
     if (s1.isBogus() || s2.isBogus()) {
-        return false;
+        content.append("<!-- ERROR could not find string(s) in POST data -->\n");
+        return;
     }
   
-    // html escape all '<' and '&' in the two strings,
-    //   in preparation for putting them into the output html.
-    UnicodeString s1Copy(s1);
-    UnicodeString s2Copy(s2);
-    s1Copy.findAndReplace("&", "&amp;");
-    s1Copy.findAndReplace("<", "&lt;");
-    s2Copy.findAndReplace("&", "&amp;");
-    s2Copy.findAndReplace("<", "&lt;");
+    // Put the two strings back into the text input field in the html page,
+    //   so that what the user typed will still be there when the page reloads.
+    //
+    storeField("%%s1a%%", s1, content);
+    storeField("%%s1b%%", s2, content);
 
-    // Convert the two input strings to utf-8 and spit them back into
-    //  the output so that the client can see them after the unescape().
-    int len = s1Copy.length() > s2Copy.length()? s1Copy.length() : s2Copy.length();
-    char *buf = new char[len*5];
-    s1Copy.extract(0, s1Copy.length(), buf, "utf-8");
-    printf("<p id=\"%s\" >%s</p>\n", name1, buf);
-    s2Copy.extract(0, s2Copy.length(), buf, "utf-8");
-    printf("<p id=\"%s\" >%s</p>\n", name2, buf);
-    delete buf;
-    buf = 0;
+    // Unescape the two input strings and put those too into the output html.
+    s2 = s2.unescape();
+    s2 = s2.unescape();
+    storeField("%%str1%%", s1, content);
+    storeField("%%str2%%", s2, content);
+
+    // Put the hex-formatted input strings into the output html.
+    //
+    UnicodeString hexS;
+    toHex(hexS, s1);
+    content.findAndReplace("%%hex1%%", hexS);
+    toHex(hexS, s2);
+    content.findAndReplace("%%hex2%%", hexS);
 
     //
     // Do the actual comparisons
     //
-    char resultString[5];
-    strcpy(resultString, "----");
+    UnicodeString binary("-");
+    UnicodeString caseless("-");
+    UnicodeString equiv("-");
+    UnicodeString caseless_equiv("-");
+
     if (s1.compare(s2) == 0) {
-        resultString[0] = 'Y';
+        binary = 'Y';
     }
     if (s1.caseCompare(s2, U_FOLD_CASE_DEFAULT) == 0) {
-        resultString[1] = 'Y';
+        caseless = 'Y';
     }
     if (Normalizer::compare(s1, s2, U_FOLD_CASE_DEFAULT, status) == 0) {
-        resultString[2] = 'Y';
+        equiv = 'Y';
     }
     if (Normalizer::compare(s1, s2, U_COMPARE_IGNORE_CASE, status) == 0) {
-        resultString[3] = 'Y';
+        caseless_equiv = 'Y';
     }
-    printf("<p id=\"%s\" >%s</p>\n", rName, resultString);
-    return true;
+    content.findAndReplace("%%binary%%", binary);
+    content.findAndReplace("%%case%%", caseless);
+    content.findAndReplace("%%equiv%%", equiv);
+    content.findAndReplace("%%eq_ca%%", caseless_equiv);
+
+    return;
 }
 
+
+//
+//  insertTemplateFile   Insert the contents of one of the html template
+//                       files into a UnicodeString in which we are building up
+//                       the html for the page.
+//
+void insertTemplateFile(UnicodeString &dest,        // Target UnicodeString
+                        char *templateFileName,     //  File name of template.
+                        char *insertPointMarker)    //  A string dest that marks the
+                                                    //    desired insertion point
+{
+    UFILE          *templFile;
+    int             fileSize;
+    const int       MAX_FILE_SIZE = 50000;
+    UChar          *buf;
+
+    templFile = u_fopen(templateFileName, "r", "", "utf-8");
+    if (templFile == NULL) {
+        dest.append("<!-- ERROR: could not open template file -->\n");
+        return;
+    }
+    buf = (UChar *)malloc(MAX_FILE_SIZE * sizeof(UChar));
+    if (buf == NULL) {
+        return;
+    }
+
+    fileSize = u_file_read(buf, MAX_FILE_SIZE, templFile);
+    if (fileSize >= MAX_FILE_SIZE || fileSize <= 0) {
+        dest.append("<!-- ERROR: could not read template file -->\n");
+        return;
+    }
+
+    if (insertPointMarker != NULL && *insertPointMarker != 0) {
+        UnicodeString ipm(insertPointMarker);
+        int insertStart = dest.indexOf(UnicodeString(ipm));
+        if (insertStart > 0) {
+            dest.replace(insertStart, ipm.length(), buf, fileSize);
+        }
+    } else {
+        dest.append(buf, fileSize);
+    }
+
+    delete buf;
+}
 
 //
 //  main()
 //
 int main(int argc, const char **argv) {
-    UErrorCode  status = U_ZERO_ERROR;
-    const char *request_method;
-    int         n;
+    UErrorCode      status = U_ZERO_ERROR;
+    const char     *request_method;
+    UnicodeString   outputText;
 
     u_init(&status); 
-    printf("Content-Type: text/html; charset=utf-8\n"
-            "\n"
-            "<html lang=\"en-US\">\n"
-            "<head>\n"
-            "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
-            "</head>\n"
-            "<body>\n");
 
+    //
+    // Read the two html template files into the UnicodeString
+    //   in which we build up the html for the generated page.
+    //   The first template file is the overall page structure, as produced
+    //   by the IBM template generator.
+    //   The second contains the page content specific to this demo app.
+    //
+    insertTemplateFile(outputText, "scompare-page-templ.html", NULL);
+    insertTemplateFile(outputText, "scompare-content-templ.html",
+        "%%space content space table here%%");
+
+ 
+    // The IBM page template does not include the css necessary for tables.
+    //   Insert the necessary css include now.
+    int32_t where = outputText.indexOf(
+        "<link rel=\"stylesheet\" type=\"text/css\" media=\"print\" href=\"//www.ibm.com/common");
+    if (where>0) {
+        outputText.insert(where,
+            "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen,print\" "
+            "href=\"//www.ibm.com/common/v14/table.css\" />\n");
+    } else {
+        outputText.append("<!-- Error in scompare.cpp while inserting table.css -->\n");
+    }
+
+
+
+    //
+    //  Fetch the POST data sent from the user's browser.
+    //  If there is none, this program was probably invoked directly, rather than
+    //    by a form submit.  In this case, provide some default data to preload the
+    //    displayed results.
+    //
+    //   Note:  the easiest way to change the default data is to run the demo,
+    //          enter the desired new defaults into the form, submit, then
+    //          in the browser, display the source of the resulting page.
+    //          In an html <!-- comment --> at the bottom will be the post data
+    //          for that submit, which can be pasted in as the new initialization string
+    //          for the postData variable, below.
+    //
     request_method = getenv("REQUEST_METHOD");
-    const char *scontentLen = getenv("CONTENT_LENGTH");
-    if (request_method==NULL || strcmp(request_method, "POST") != 0 || scontentLen==NULL) {
-        printf("Error!");
-        exit(-1);
-    }
-    int contentLen = atoi(scontentLen);
-    if (contentLen<=0 || contentLen>=10000) {
-        printf("Error - bad content length!");
-        exit(-1);
-    }
-
-    char *content = new char[contentLen+20];
-    fread(content, 1, contentLen, stdin);
-    content[contentLen] = 0;
-    
-    for (n=1; ; n++) {
-        char sAName[20];
-        char sBName[20];
-        char rName[20];
-        sprintf(sAName, "s%da", n);
-        sprintf(sBName, "s%db", n);
-        sprintf(rName,  "r%d", n);
-
-        if (doCompare(content, sAName, sBName, rName) == false) {
-            break;
+    const char *sPostDataLen = getenv("CONTENT_LENGTH");
+    const char *postData = "s1a=%CE%B1%CE%BB%CF%86%CE%B1&s1b=%CE%B1%CE%BB%CF%86%CE%B1";
+    size_t postDataLen = strlen(postData);  // Default values
+    char *allocatedContent = 0;
+    if (request_method!=NULL && (strcmp(request_method, "POST")==0) && sPostDataLen!=NULL) {
+        // Real user data from form submit.  Read it in.
+        size_t t = atoi(sPostDataLen);
+        if (t<=0 || t>=10000) {
+            outputText.append("<!-- POST data length was bad -->\n");
+        } else {
+            postDataLen = t;
+            allocatedContent = new char[postDataLen+20];
+            fread(allocatedContent, 1, postDataLen, stdin);
+            allocatedContent[postDataLen] = 0;
+            postData = allocatedContent;   
         }
     }
+    
 
-    printf("\n</body>\n");
-    delete[] content;
+    //
+    //  Here is the actual string compare, including stuffing the results
+    //    into the various %%fields%% in the html template.
+    //
+    doCompare(postData, outputText);
+    
+    //
+    //  Write the completed html page to stdout.
+    //
+    UFILE *u_stdout = u_finit(stdout, "", "utf-8");
+    u_fprintf(u_stdout, "Content-Type: text/html; charset=utf-8\n\n");
+    const UChar *htmlBuf = outputText.getBuffer();
+    u_file_write(htmlBuf, outputText.length(), u_stdout);
+    // Debugging aid - put the raw post data back into the generated html,
+    //                 as an html comment.
+    u_fprintf(u_stdout, "<!--%s-->\n", postData);
+    u_fclose(u_stdout);
+
+    delete[] allocatedContent;
     u_cleanup();
+
     return 0;
+
 }
 
 
