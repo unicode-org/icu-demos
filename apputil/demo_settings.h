a21930cd5d67692fa479a2f6af353ab05d28ae76
George Rhoten <grhoten@users.noreply.github.com>
2005-05-24 20:13:24 +0000

    [ICU-4358] Add a common header for all demos.
    X-SVN-Rev: 17657

---
+++ apputil/demo_settings.h
@@ -0,0 +1,27 @@
+/*
+*******************************************************************************
+*
+*   Copyright (C) 2005-2005, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+*
+*******************************************************************************
+*   file name:  demo_settings.h
+*   encoding:   US-ASCII
+*   tab size:   4 (not used)
+*   indentation:4
+*
+*   created on: 2005May24
+*   created by: George Rhoten
+*
+*   These are the settings that are common to all demos.
+*
+*/
+
+#ifndef DEMO_SETTINGS_H
+#define DEMO_SETTINGS_H 1
+
+DEMO_COMMON_MASTHEAD "data/common-masthead.html"
+DEMO_COMMON_LEFTNAV "data/common-leftnav.html"
+DEMO_COMMON_FOOTER "data/common-footer.html"
+
+#endif
