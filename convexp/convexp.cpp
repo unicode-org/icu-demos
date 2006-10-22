237b2eb4ec792d8b3f5d2e78dc5695298b4ec4ee
George Rhoten <grhoten@users.noreply.github.com>
2006-10-22 06:19:00 +0000

    [ICU-5460] Fix HTML layout
    X-SVN-Rev: 20581

--- convexp/convexp.cpp
+++ convexp/convexp.cpp
@@ -125,10 +125,9 @@ static const char endForm[]=
 
     "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"175\" class=\"v14-gray-table-border\">\n"
     "<tr><th colspan=\"3\" class=\"v14-header-1-small\">Related Topics</th></tr>\n"
-    "<tr><td colspan=\"3\"><ul>\n"
-    "<li><a style=\"align: left; white-space: nowrap\" href=\"http://icu.sourceforge.net/docs/demo/convexp_help.html\">" PROGRAM_NAME " Help</a></li>\n"
-    "<li><a style=\"align: left; white-space: nowrap\" href=\"http://icu.sourceforge.net/charts/charset/\">ICU Charset Information</a></li>\n"
-    "</ul></td></tr>\n"
+    "<tr><td colspan=\"3\"><span style=\"font-size: 5px\">"NBSP"</span></td></tr>\n"
+    "<tr><td align=\"right\" valign=\"top\">"NBSP"</td><td align=\"left\">&middot;"NBSP"<a href=\"http://icu.sourceforge.net/docs/demo/convexp_help.html\">" PROGRAM_NAME " Help</a></td><td width=\"5\">" NBSP "</td></tr>\n"
+    "<tr><td align=\"right\" valign=\"top\">"NBSP"</td><td align=\"left\">&middot;"NBSP"<a href=\"http://icu.sourceforge.net/charts/charset/\">ICU Charset Information</a></td><td width=\"5\">" NBSP "</td></tr>\n"
     "<tr><td><span style=\"font-size: 5px\">"NBSP"</span></td></tr>\n"
     "</table>\n"
 
