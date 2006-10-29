b8147a51f0e3678e3b61ee2a4c2bc2227d923d14
George Rhoten <grhoten@users.noreply.github.com>
2006-10-29 22:44:14 +0000

    [ICU-5460] Improve UI compatibility with FireFox
    X-SVN-Rev: 20613

--- convexp/printcp.cpp
+++ convexp/printcp.cpp
@@ -334,7 +334,7 @@ void printCPTable(UConverter *cnv, char *startBytes, UErrorCode *status) {
     }
     puts("<br /><br />\n"
         "<table width=\"100%\" border=\"1\" cellspacing=\"0\" class=\"data-table-2\" summary=\"A 16 by 16 table with headers on each side of the table representing the nibble of a byte.\">\n"
-        "<caption><em><a name=\"layout\">Codepage Layout</a></em></caption>");
+        "<caption><em><a name=\"layout\"></a>Codepage Layout</em></caption>");
     printf("<tr><td colspan=\"18\">");
 
     switch (convType) {
