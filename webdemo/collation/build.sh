f914a3960c868da53f66099d677c265378212fa3
Markus Scherer <markus.icu@gmail.com>
2016-10-26 22:10:38 +0000

    [ICU-12814] update the list of collation tailorings for CLDR 30/ICU 58; requires bug fix [ICU-12827] r39484
    X-SVN-Rev: 39485

--- webdemo/collation/build.sh
+++ webdemo/collation/build.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
-ICU_SRC=/home/mscherer/svn.icu/trunk/src
-PREFIX=/home/mscherer/svn.icu/trunk/inst
-ICUAPPS_SRC=/home/mscherer/svn.icuapps/trunk/src
+HOME=/usr/local/google/home/mscherer
+ICU_SRC=$HOME/svn.icu/trunk/src
+PREFIX=$HOME/svn.icu/trunk/inst
+ICUAPPS_SRC=$HOME/svn.icuapps/trunk/src
 CGI_BIN=/var/www/cgi-bin
 
-CC=clang
 CXX=clang++
-CFLAGS="-g -std=c99 -Wall -pedantic -Wshadow -Wpointer-arith -Wmissing-prototypes -Wwrite-strings"
 CXXFLAGS="-g --std=c++0x -W -Wall -pedantic -Wpointer-arith -Wwrite-strings -Wno-long-long -DU_USING_ICU_NAMESPACE=0"
 CPPFLAGS="-DU_NO_DEFAULT_INCLUDE_UTF_HEADERS=1 -DU_DEBUG=1 -D_REENTRANT -DU_HAVE_ELF_H=1 -DUNISTR_FROM_CHAR_EXPLICIT=explicit -DUNISTR_FROM_STRING_EXPLICIT= -I$ICU_SRC/source/common -I$ICU_SRC/source/i18n -I$ICU_SRC/source/tools/toolutil"
 LINKFLAGS="-L$PREFIX/lib -licutu -licui18n -licuuc -licudata -lpthread -ldl -lm"
@@ -15,10 +14,10 @@ $CXX $CPPFLAGS $CXXFLAGS $LINKFLAGS $ICUAPPS_SRC/webdemo/collation/available.cpp
 LD_LIBRARY_PATH=$PREFIX/lib $PREFIX/bin/available > $ICUAPPS_SRC/../available-collators.txt
 
 $CXX $CPPFLAGS $CXXFLAGS $LINKFLAGS $ICUAPPS_SRC/webdemo/collation/sortcgi.cpp -o $PREFIX/bin/sortcgi
-cp $PREFIX/bin/sortcgi $CGI_BIN/collation
-chmod a+rx $CGI_BIN/collation/sortcgi
+# cp $PREFIX/bin/sortcgi $CGI_BIN/collation
+# chmod a+rx $CGI_BIN/collation/sortcgi
 
-cp webdemo/collation/index.html /var/www/collation.html
-chmod a+r /var/www/collation.html
+# cp webdemo/collation/index.html /var/www/collation.html
+# chmod a+r /var/www/collation.html
 
 # ~/svn.icuapps/trunk/src$ webdemo/collation/build.sh
