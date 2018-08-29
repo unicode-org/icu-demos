f68324fa9add6e3c204b6393f1419e85f10c5615
Steven R. Loomis <srl295@fahmu.net>
2018-08-29 11:08:07 -0600

    make it work

--- iucsamples/c/iucsamples.h
+++ iucsamples/c/iucsamples.h
@@ -6,3 +6,8 @@
 #define ASSERT_OK(x) if(U_FAILURE(x)) { fprintf(stderr,"%s:%d: ICU Failure %s\n", __FILE__, __LINE__, u_errorName(x)); }
 
 #endif
+
+// #ifdef CPLUSPLUS
+#include <unicode/utypes.h>
+using namespace icu;
+// #endif
