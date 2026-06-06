7fe87500b3d1f5b91c072a359cdc6254b090b9f7
Mihai Nita <nmihai_2000@yahoo.com>
2026-06-05 17:31:36 -0700

    Update deployment documentation

---
+++ icu-kube/push-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+
+# Update ICU_VER for each new release.
+# It should match the one in build-deploy.sh 
+export ICU_VER=78.3
+
+docker push us-central1-docker.pkg.dev/goog-unicode-dev/unicode-jsps/icu4c-demos:${ICU_VER}
--- icu4jweb/build-deploy.sh
+++ icu4jweb/build-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -x
-# the project id "icu4c-demos" cannot be changed.
-# Update the final part of the tag for each new release.
-docker build -t us-central1-docker.pkg.dev/goog-unicode-dev/unicode-jsps/icu4j-demos:78.3 .
+
+# Update ICU_VER for each new release.
+# It should match the version part of the release number in GitHub (without the `release-` part)
+# https://github.com/unicode-org/icu/releases
+
+export ICU_VER=78.3
+
+docker build -t us-central1-docker.pkg.dev/goog-unicode-dev/unicode-jsps/icu4j-demos:${ICU_VER} .
